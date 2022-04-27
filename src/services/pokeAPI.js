8c9fabbd14ba87b34a372f89831f233da770b0eb
darrubla <darrubla@uninorte.edu.co>
2022-04-27 15:15:43 -0500

    utils first elaboration

---
+++ src/services/pokeAPI.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+export const getListado = (offset = 0) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${offset}`
+  return axios({
+    method: 'GET',
+    url
+  })
+}
+
+export const getPokeInfo = (url) => {
+  return axios({
+    method: 'GET',
+    url
+  })
+}
+
+export const getUrl = (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+  return url
+}
+
+
+export const getPokeEntry = (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon-species/${id}/`
+  return axios({
+    method: 'GET',
+    url
+  })
+}
